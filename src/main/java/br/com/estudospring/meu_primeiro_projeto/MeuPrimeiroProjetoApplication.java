217f974dff76eb722c7387510184987151759c7e
Gilson de lima <>
2024-08-25 12:51:10 -0300

    first commit

---
+++ src/main/java/br/com/estudospring/meu_primeiro_projeto/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,23 @@
+package br.com.estudospring.meu_primeiro_projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+	@GetMapping("/hello")	
+	public String helloWord(){
+		return "Hello World!";
+	}
+
+}
